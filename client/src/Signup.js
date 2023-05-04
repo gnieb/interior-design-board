e3c5c53a2d5813ddcbbd8092cdadff1d004117f6
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-04 17:36:14 -0400

    SignUp Styling with MAterial UI

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -7,6 +7,32 @@ import Button from '@mui/material/Button';
 import Form from 'react-bootstrap/Form';
 import Alert from '@mui/material/Alert';
 import AlertTitle from '@mui/material/AlertTitle';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" >
+      {'Copyright © '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
 
 export default function Signup() {
     const { setDesigner} = useContext(UserContext)
@@ -60,8 +86,37 @@ export default function Signup() {
     })
 
     return (
-        <>
-        <p>Required fields *</p>
+        <ThemeProvider theme={theme}>
+          <Grid container component="main" sx={{ height: '100vh' }}>
+          <CssBaseline />
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
+            sx={{
+              backgroundImage: 'url(https://plus.unsplash.com/premium_photo-1680632914285-0bc6110e475c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60)',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+          <p>Required fields *</p>
         <Form onSubmit= {formik.handleSubmit} >
             <Form.Label>First Name *</Form.Label>
             <Form.Control type='text' name="first_name" value={formik.values.first_name} onChange={formik.handleChange} />
@@ -86,6 +141,10 @@ export default function Signup() {
              <p style={{ color: "red" }}> {formik.errors.password}</p>
             <Button type = 'submit'>Sign Up</Button>
         </Form> 
-        </>
+        <Copyright sx={{ mt: 5 }} />
+        </Box>
+        </Grid>
+        </Grid>
+        </ThemeProvider>
     )   
 }
