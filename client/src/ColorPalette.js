dd5e4bd2f98a60f617d5d23c58ac8d5d693e7d24
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-04 16:46:48 -0400

    ColorPalette component created and moved into Grid Container with moodboard

--- client/src/ColorPalette.js
+++ client/src/ColorPalette.js
@@ -1,52 +1,23 @@
-import Moodboard from "./Moodboard"
-import ColorPalette from "./ColorPalette";
+
 import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
-import Modal from 'react-bootstrap/Modal';
 import DesignEdit from "./DesignEdit";
-import DeleteIcon from '@mui/icons-material/Delete';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
 
-export default function DesignDisplay ({ piecesLibrary, setPiecesLibrary, d, removeDesign, addNewPiece}) {
-    const [showModal, setShowModal] = useState(false)
-    const [editMode, setEditMode] = useState(false)
-    const handleShow = () => setShowModal(true)
-    const handleClose = () => setShowModal(false)
-    const [showAssocPD, setShowAssocPD] = useState([])
-    
-    const handleAssociatedPD = (p) => setShowAssocPD([...showAssocPD, p])
-    
-    const handleRemovePiece = (pObj) => {
-        const updatedPieces = showAssocPD.filter(pd => pd !== pObj )
-        setShowAssocPD(updatedPieces)
-    }
 
+
+
+
+export default function ColorPalette ({d}) {
     const [color1, setColor1] = useState(d.color1 ? d.color1 : "#EEEEEE" )
     const [color2, setColor2] = useState(d.color2 ? d.color2 : "#CCCCCC")
     const [color3, setColor3] = useState(d.color3 ? d.color3 : "#999999")
     const [color4, setColor4] = useState(d.color4 ? d.color4 : "#666666")
     const [color5, setColor5] = useState(d.color5 ? d.color5 : "#333333")
-
-    /////////////////////// 2 separate things://////////
-    //1. randome geneator: coming from color palette API: comes in an array of 5 arrays
-    // => translate that into rbgtoHex colors and distribute them to the boxes. 
-
-    //2. saving to the database and setting randompalette though... that will be strings. why not just store these as hex string??
-
-    useEffect(() => {
-        fetch(`/designs/${d.id}`)
-            .then(r => {
-                if (r.ok) {
-                    r.json().then( r => {
-                        setShowAssocPD(r.pdinstances)
-                    })
-                }
-            })
-    }, [])
 
     const generateRandomPalette = () => {
         fetch('/randpalette')
@@ -61,12 +32,10 @@ export default function DesignDisplay ({ piecesLibrary, setPiecesLibrary, d, rem
             })
     }
 
-
-    
     const toHex = (color) => {
-           const hex = color.toString(16)
-           return hex.length == 1 ? "0" + hex : hex
-        }
+        const hex = color.toString(16)
+        return hex.length == 1 ? "0" + hex : hex
+     }
     const rgbToHex = (array) => `#${toHex(array[0])}${toHex(array[1])}${toHex(array[2])}`
     
     const setIndividualColors = (palette) => {
@@ -76,7 +45,6 @@ export default function DesignDisplay ({ piecesLibrary, setPiecesLibrary, d, rem
         setColor4(rgbToHex(palette[3]))
         setColor5(rgbToHex(palette[4]))
     }
-    
 
     const handleSavePalette = () => {
         console.log("SAVINGGGGGG")
@@ -95,60 +63,15 @@ export default function DesignDisplay ({ piecesLibrary, setPiecesLibrary, d, rem
         .then(r=> {
             if(r.ok) {
                 console.log(r)
-                // setIndividualColors(newPal)
             } else {
                 console.log(r.status)
             }
         })
     }
-    
-    const handleCloseAndDelete = (e) => {
-        fetch(`/designs/${d.id}`, {
-            method: "DELETE"
-        })
-        .then( r => {
-            if (r.ok) {
-              removeDesign(d)
-              console.log(r.status)  
-            } else {
-                console.log(r.status)
-            }
-        })
-    }
-
-    const handleEdit = () => setEditMode(!editMode)
+ 
 
     return (
-        <>
-        <h2>{d.name}</h2>
-        <Button onClick={handleEdit}>{editMode ? "View Moodboard" :"Edit design"}</Button>
-        
-        {editMode ?
-        <>
-        <Button onClick={handleShow} variant="outlined" color="error" startIcon={<DeleteIcon />}>
-        Delete
-      </Button>
-        <DesignEdit handleAssociatedPD={handleAssociatedPD}
-        showAssocPD={showAssocPD}
-         handleRemovePiece={handleRemovePiece}/> 
-         <Modal show={showModal} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>DELETE {d.name.toUpperCase()}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>ARE YOU SURE YOU WANT TO DELETE THIS DESIGN?</Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    I CHANGED MY MIND
-                </Button>
-                
-                <Button onClick={handleCloseAndDelete} variant="outlined" color="error" startIcon={<DeleteIcon />}>
-                I'm Sure
-                </Button>
-            </Modal.Footer>
-            </Modal>
-         </> :
-       <>
-{/*     
+        <div style={{padding:"50px"}}>
         <Box 
                 sx={{
                     width: 200,
@@ -213,24 +136,9 @@ export default function DesignDisplay ({ piecesLibrary, setPiecesLibrary, d, rem
               </IconButton>
             <p>How to use the palettes: click the generate button to see new palette suggestions. Only click the save button if you'd like to replace your old palette.</p>
             <Button onClick={generateRandomPalette}>Generate Random Palette</Button>
-            <Button onClick={handleSavePalette}>Save Palette</Button> */}
-        <Grid container > 
-            <Grid item xs={4}>
-                <ColorPalette d={d} />
-            </Grid>
-            <Grid item xs={8}>
-                <Moodboard 
-                addNewPiece={addNewPiece} 
-                d={d} 
-                handleAssociatedPD={handleAssociatedPD} 
-                showAssocPD={showAssocPD}
-                setPiecesLibrary={setPiecesLibrary}
-                piecesLibrary={piecesLibrary}/>
-            </Grid>
-        </Grid>   
-        </> 
-        
-        }
-        </> 
+            <Button onClick={handleSavePalette}>Save Palette</Button>
+            
+        </div>
+
     )
-} 
+}
--- client/src/Moodboard.js
+++ client/src/Moodboard.js
@@ -7,6 +7,7 @@ import Modal from 'react-bootstrap/Modal';
 import Form from 'react-bootstrap/Form';
 
 
+
 export default function Moodboard ({piecesLibrary, setPiecesLibrary, addNewPiece, d, handleAssociatedPD, showAssocPD}) {
     const {designer} = useContext(UserContext)
     const [showSecondModal, setShowSecondModal] = useState(false)
