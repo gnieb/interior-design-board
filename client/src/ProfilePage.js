d9dc13fb8135c077dcbdfecb943caea6c9ef601c
Grace Nieboer <grace.nieboer@outlook.com>
2023-05-08 00:14:42 -0400

    Profile component styling

--- client/src/ProfilePage.js
+++ client/src/ProfilePage.js
@@ -2,6 +2,10 @@ import UpdateProfilePage from "./UpdateProfilePage"
 import { UserContext } from "./context/user"
 import { useContext, useState } from "react"
 import Menu from "./Menu"
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
 
 
 export default function ProfilePage () {
@@ -19,15 +23,27 @@ export default function ProfilePage () {
         <>
         {editMode ?
         <UpdateProfilePage handleEditMode={handleEditMode} /> :
-       (<>
+       (<div id="profile">
        <Menu />
-       <h2>My Profile</h2>
-        <h3>First name - {first_name}</h3>
-        <h3>Last name - {last_name}</h3>
-        <h3>Email - {email}</h3>
-        <h3>Location - {city}</h3>
-        <button onClick={handleEditMode}>Edit Designer</button>
-        </> )}
+       <Grid container component="main" >
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} 
+            sx={{
+                margin:'10px'
+            }}>
+            <Box
+            sx={{color:'#263A29', 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'left',}}>
+            <h2><strong>PROFILE</strong></h2>
+            <h3>NAME {first_name} {last_name}</h3>
+            <h3>EMAIL - {email}</h3>
+            <h3>LOCATION - {city}</h3>
+            <Button onClick={handleEditMode}>Edit</Button>
+            </Box>
+        </Grid>
+        </Grid>
+        </div> )}
         </>
     )
 }
